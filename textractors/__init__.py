673f0229972ffdcf07e58118ccab33535d367b9e
Krishnan Narayan <knarayan@paloaltonetworks.com>
2024-02-27 17:25:07 -0800

    First commit

---
+++ textractors/__init__.py
@@ -0,0 +1,7 @@
+# package to deal with text extraction from text/pdf/image etc
+import os
+
+
+def text_extractor(data_location):
+    # todo: add support for pdf and other file types
+    return data_location
